02fe584add49608ee8638d99dded935bd0ebea6a
den1k0n <dkondratiuk06@gmail.com>
2022-05-18 18:04:11 +0200

    add ui

--- main.js
+++ main.js
@@ -1,36 +1,67 @@
-const doStuff = pos => console.log(pos);
-const handleErr = err => console.log(err);
+const startPreloader = () => {
+    window.setTimeout(() => {document.querySelector('.preloader').style.display = 'none'; document.querySelector('.container').style.display = 'initial'}, 2000);
+}
 
+// window.onload = startPreloader;
 // You didn't see anything
 const
     defaultApiKey = '0c5e131ca9786828236c6fd421583751',
     testApiKey = '8423e60a5000bca50b7e3468a8cae03b';
 
+// CACHE DOM
+const
+    container = document.querySelector('.container');
+    input = container.querySelector('input'),
+    searchBtn = document.getElementById('search'),
+    currentTemp = document.getElementById('currentTemp'),
+    feelsLike = document.getElementById('feelsLike'),
+    cityName = document.getElementById('cityName');
 
-let city = 'frankfurt',
+let
     lat,
-    lon;
+    lon,
+    cityOutput;
 
+// API URL for city call
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+// API URL for lat/lon call
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
 const createUrl = (apiKey, city = 'undefined', lat = 'undefined', lon = 'undefined') => {
     if(city !== 'undefined') return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 }
 
 const showData = data => {
     console.log(data);
+    cityName.textContent = cityOutput;
+    currentTemp.textContent = data.current.temp + ' °C';
+    feelsLike.textContent = data.current.feels_like + ' °C';
 }
 
 const secondFetch = res => {
-    // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-    console.log(lat, lon)
+    cityOutput = res.name;
+    console.log(res);
+    lat = res.coord.lat;
+    lon = res.coord.lon;
     fetch(createUrl(testApiKey, 'undefined', lat, lon))
     .then(response => response.json())
     .then(result => showData(result))
     .catch(err => console.log(err));
 }
 
-fetch(createUrl(defaultApiKey, city))
+const fetching = () => {
+    city = input.value;
+    fetch(createUrl(defaultApiKey, city))
     .then(response => response.json())
     .then(result => secondFetch(result))
     .catch(err => console.log(err));
+    container.style.display = 'none';
+    document.querySelector('.preloader').style.display = 'initial';
+    startPreloader();
+}
+searchBtn.addEventListener('click', fetching);
+window.addEventListener('keydown', (ev) => {
+    if(ev.key === 'Enter') fetching();
+})
