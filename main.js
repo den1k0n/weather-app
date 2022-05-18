ca5c5fcea1c949ec156ca2366c467887324aa547
den1k0n <dkondratiuk06@gmail.com>
2022-05-18 18:41:21 +0200

    add user's geolocation detection

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ const
     searchBtn = document.getElementById('search'),
     currentTemp = document.getElementById('currentTemp'),
     feelsLike = document.getElementById('feelsLike'),
-    cityName = document.getElementById('cityName');
+    cityName = document.getElementById('cityName'),
+    desc = document.getElementById('description');
 
 let
     lat,
@@ -29,15 +30,23 @@ let
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 const createUrl = (apiKey, city = 'undefined', lat = 'undefined', lon = 'undefined') => {
-    if(city !== 'undefined') return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    if(city !== 'undefined') return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 }
 
+// const showData = data => {
+//     console.log(data);
+//     cityName.textContent = cityOutput;
+//     currentTemp.textContent = data.current.temp + ' °C';
+//     feelsLike.textContent = data.current.feels_like + ' °C';
+// }
+
 const showData = data => {
     console.log(data);
-    cityName.textContent = cityOutput;
-    currentTemp.textContent = data.current.temp + ' °C';
-    feelsLike.textContent = data.current.feels_like + ' °C';
+    cityName.textContent = data.name;
+    desc.textContent = data.weather[0].description;
+    currentTemp.textContent = data.main.temp + ' °C';
+    feelsLike.textContent = data.main.feels_like + ' °C';
 }
 
 const secondFetch = res => {
@@ -51,12 +60,25 @@ const secondFetch = res => {
     .catch(err => console.log(err));
 }
 
+// const fetching = () => {
+//     city = input.value;
+//     fetch(createUrl(defaultApiKey, city))
+//     .then(response => response.json())
+//     .then(result => secondFetch(result))
+//     .catch(err => console.log(err));
+//     container.style.display = 'none';
+//     document.querySelector('.preloader').style.display = 'initial';
+//     startPreloader();
+// }
+
 const fetching = () => {
     city = input.value;
     fetch(createUrl(defaultApiKey, city))
     .then(response => response.json())
-    .then(result => secondFetch(result))
+    .then(result => showData(result))
     .catch(err => console.log(err));
+
+
     container.style.display = 'none';
     document.querySelector('.preloader').style.display = 'initial';
     startPreloader();
@@ -65,3 +87,42 @@ searchBtn.addEventListener('click', fetching);
 window.addEventListener('keydown', (ev) => {
     if(ev.key === 'Enter') fetching();
 })
+
+
+
+
+
+
+// geo
+const button = document.querySelector("#geoBtn");
+button.addEventListener("click", ()=>{
+    if(navigator.geolocation){
+        button.innerText = "Allow to detect location";
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        button.innerText = "Your browser not support";
+    }
+});
+function onSuccess(position){
+    button.innerText = "Detecting your location...";
+    let {latitude, longitude} = position.coords;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=YOUR_API_KEY`)
+    .then(response => response.json()).then(response =>{
+        let allDetails = response.results[0].components;
+        console.table(allDetails);
+        let {county, postcode, country} = allDetails;
+        button.innerText = `${county} ${postcode}, ${country}`;
+    }).catch(()=>{
+        button.innerText = "Something went wrong";
+    });
+}
+function onError(error){
+    if(error.code == 1){
+        button.innerText = "You denied the request";
+    }else if(error.code == 2){
+        button.innerText = "Location is unavailable";
+    }else{
+        button.innerText = "Something went wrong";
+    }
+    button.setAttribute("disabled", "true");
+}
